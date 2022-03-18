f837e8fc535e64a595588f71fef730e75b947324
baranaydogan <baran.aydogan@gmail.com>
2022-03-18 12:51:47 +0200

    added new feature for seed fix

--- build/src/CMakeFiles/rttvis.dir/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/rttvis.dir/qrc_resources.cpp.o: \
- /home/baran/Work/code/realTimeTractogramVisualizer/build/src/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/src/CMakeFiles/rttvis.dir/rttvis_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/rttvis.dir/rttvis_autogen/mocs_compilation.cpp.o: \
- /home/baran/Work/code/realTimeTractogramVisualizer/build/src/rttvis_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
